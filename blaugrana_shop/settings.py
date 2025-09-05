6998494b7d815772ceca3638d1c4dfbab9fab5c9
sheriqa <andisheriqadewina@gmail.com>
2025-09-05 16:25:36 +0700

    Complete tutorial 2: Django MVT implementation

--- blaugrana_shop/settings.py
+++ blaugrana_shop/settings.py
@@ -33,7 +33,6 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "a-sheriqa-blaugranashop.pbp.cs.ui.ac.id"]
 
 # Application definition
-#test
 
 INSTALLED_APPS = [
     'django.contrib.admin',
